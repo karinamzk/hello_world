b4961401806e882b786646789ddf8c5643b9df7c
Karina Akina <karinamzk2@gmail.com>
2023-09-21 18:05:49 -0300

    Atividade

---
+++ HelloWord/Helloword/Controllers/HelloController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Helloword.Controllers
+{
+    [Route("~/")]
+    [ApiController]
+    public class HelloController
+    {
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello Turma .NET 03!";
+        }
+
+        [HttpGet("/bsm")]
+        public string Bsm()
+        {
+            return "Responsabilidade Pessoal \n Mentalidade de crescimento \n Orientação ao futuro \n Persistência \n Comunicação \n Orientação aos Detalhes \n Trabalho em equipe \n Proatividade";
+        }
+
+        [HttpGet("/objetivos")]
+        public string Objetivos()
+        {
+            return "ASP.NET \nSQL";
+        }
+    }
+}
